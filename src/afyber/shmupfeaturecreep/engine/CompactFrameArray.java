ed6d71dc6ee2763d775d5443fb3c7eb06ebd5833
africalinguist <26539285+afyber@users.noreply.github.com>
2021-11-26 11:36:01 -0500

    Just in case

--- src/afyber/shmupfeaturecreep/engine/CompactFrameArray.java
+++ src/afyber/shmupfeaturecreep/engine/CompactFrameArray.java
@@ -1,3 +1,29 @@
 package afyber.shmupfeaturecreep.engine;
 
-public record CompactFrameArray(int[] a, int dataWidth) {}
+import java.util.Arrays;
+import java.util.Objects;
+
+public record CompactFrameArray(int[] a, int dataWidth) {
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		CompactFrameArray that = (CompactFrameArray)o;
+		return dataWidth == that.dataWidth && Arrays.equals(a, that.a);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(dataWidth);
+		result = 31 * result + Arrays.hashCode(a);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "CompactFrameArray{" +
+				"a=" + Arrays.toString(a) +
+				", dataWidth=" + dataWidth +
+				'}';
+	}
+}
