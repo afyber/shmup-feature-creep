735ea566d4250225c0dce3ab89d3b332bb4b6626
afyber <26539285+afyber@users.noreply.github.com>
2022-02-16 08:37:39 -0500

    Final improvement to logging

--- src/afyber/shmupfeaturecreep/EntryPoint.java
+++ src/afyber/shmupfeaturecreep/EntryPoint.java
@@ -6,11 +6,14 @@ import afyber.shmupfeaturecreep.engine.screen.Screen;
 public class EntryPoint {
 
 	public static void main(String[] args) {
-		// this is just to ensure that errors are logged
+		// this is just to ensure that errors and exceptions are logged
 		try {
 			Main.mainLoop();
 		} catch (Exception e) {
 			Main.LOGGER.log(EngineLogger.Level.ERROR, "Unhandled exception occurred in mainLoop:", e);
+		} catch (Error e) {
+			Main.LOGGER.log(EngineLogger.Level.ERROR, "Error occurred in mainLoop:", e);
+			throw e;
 		} finally {
 			if (!Screen.isWindowClosed()) {
 				Main.LOGGER.log(EngineLogger.Level.INFO, "Emergency window dispose called");
